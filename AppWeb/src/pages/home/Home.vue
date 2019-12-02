07888f8373d7b62d663b96828d1d9b4a65bf69a5
Monaka <845404239@qq.com>
2019-12-02 11:37:09 +0800

    2019/12/2 オオミス　index-ajax

--- AppWeb/src/pages/home/Home.vue
+++ AppWeb/src/pages/home/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <home-header></home-header>
-    <home-swiper></home-swiper>
-    <home-icons></home-icons>
-    <home-hot></home-hot>
+    <home-header :city='city'></home-header>
+    <home-swiper :list='swiperList'></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-hot :list='hotList'></home-hot>
+    <home-liked :list='likeList'></home-liked>
+    <home-weekend :list='weekendList'></home-weekend>
   </div>
 </template>
 
@@ -12,8 +14,10 @@
   import HomeSwiper from './components/Swiper'
   import HomeIcons from './components/Icons'
   import HomeHot from './components/Hot'
-  import HomeLiked from './components/Liked'
-  import HomeWeekend from './components/Weekend'
+  import HomeLiked from './components/like'
+  import HomeWeekend from './components/weekend'
+  import axios from 'axios'
+
   export default {
     name: 'Home',
     components: {
@@ -23,6 +27,37 @@
       HomeHot,
       HomeLiked,
       HomeWeekend
+    },
+    data () {
+      return {
+        city:'',
+        swiperList: [],
+        iconList: [],
+        hotList: [],
+        likeList: [],
+        weekendList: []
+      }
+    },
+    methods: {
+      getHomeInfo () {
+        axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      },
+      getHomeInfoSucc (res) {
+            res = res.data
+            if(res.ret && res.data){
+              // 判定res.ret返回是否真并且数据是否存在
+              const data = res.data
+              this.city = data.city
+              this.swiperList = data.swiperList
+              this.iconList = data.iconList
+              this.hotList = data.hotList
+              this.likeList = data.likeList
+              this.weekendList = data.weekendList
+            }
+      }
+    },
+    mounted () {
+      this.getHomeInfo()
     }
   }
 </script>
---
+++ AppWeb/src/pages/home/componets/Hot.vue
@@ -0,0 +1,85 @@
+<
+<template>
+  <div>
+    <div class="title">
+      <img class="title-img" src="http://img1.qunarzz.com/piao/fusion/1711/16/bfbb9874e8f11402.png">
+      <span class='title-left'>本周热门榜单</span>
+      <span class="title-right">全部榜单 ></span>
+    </div>
+    <div class="wrapper">
+      <swiper :options='swiperOption'>
+        <!-- hotList修改为list -->
+        <swiper-slide v-for='item of list' :key='item.id'>
+          <div class="hot-img">
+            <img class="hot-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="desc">{{item.desc}}</p>
+          <p class="price">{{item.price}}<span>起</span></p>
+        </swiper-slide>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeHot',
+    props: {
+      list: Array
+      // props接受list并指定类型为数组
+    },
+    data() {
+      return {
+        swiperOption: {
+          slidesPerView: 3.5,
+          centeredSlides: false
+        }
+        // 删掉hotList数组
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl' .title position relative padding .2rem padding-bottom -.1rem .title-left font-size .32rem;
+  line-height .44rem
+  .title-img
+  display: inline-block;
+  overflow: hidden;
+  width: .3rem;
+  height: .3rem;
+  vertical-align: top;
+  .title-right
+  float right
+  color: #616161;
+  font-size: .24rem;
+  line-height: .28rem;
+  position: absolute;
+  top: .36rem;
+  right: .32rem;
+  .wrapper
+  height 0
+  padding-bottom 40%
+  overflow hidden
+  width: 100%
+  .hot-img
+  overflow hidden
+  height 24%
+  margin-top -.2rem
+  .hot-img-content
+  margin .15rem
+  width: 100%
+  .desc
+  text-align center
+  font-size .24rem
+  margin-top .12rem
+  line-height: .32rem
+  color #212121
+  ellipsis()
+  .price
+  text-align center
+  color #ff8300
+  span
+  color #212121
+  font-size .24rem
+</style>
